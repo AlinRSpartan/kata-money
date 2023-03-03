40077a9d20e1737cb17d6be5baa6cfd083ca1ba4
Ruya Kumru-Holroyd <ruya.kh@outlook.com>
2023-03-03 14:56:28 +0000

    method complete and working, created unit test project

--- KataMoney/KataMoney/Program.cs
+++ KataMoney/KataMoney/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace KataMoney
+﻿namespace KataMoney;
+
+public class Moneyy
 {
-    public class Program
+    static void Main()
+    {   
+        double money = 5.47;
+        
+        //int totalPennies = (int)(money * 100);
+        //Console.WriteLine(totalPennies + "pennies");
+
+        //int noOf5Notes = totalPennies / 500;
+        //Console.WriteLine(noOf5Notes);
+
+        Console.WriteLine(GetMoney(money));
+    }
+
+    public static string GetMoney(double money)
     {
-        static void Main()
+        int totalPennies = (int)(money * 100);
+
+        int[] storeValues = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+        int[] noteValue = { 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1 };
+        for(int i = 0; i < storeValues.Count(); i++)
         {
-            Console.WriteLine("Hello Alin");
+            storeValues[i] = totalPennies / noteValue[i];
+            totalPennies = totalPennies % noteValue[i];
         }
+
+        string[] notesString = { "£50", "£20", "£10", "£5", "£2", "£1", "50p", "20p", "10p", "5p", "2p", "1p" };
+
+        string result = "";
+
+        for(int i = 0; i < storeValues.Count(); i++)
+        {
+            if (storeValues[i] > 0)
+                result += ($"{storeValues[i]} {notesString[i]}\n");
+        }
+        return result;
     }
 }
---
+++ KataMoney/KataMoneyTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+using KataMoney;
+
+namespace KataMoneyTest;
+
+public class MoneyyTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
